97aad763971441e9899de5c958eff62ab958aaa7
ultranikit <ultranikit@gmail.com.com>
2020-05-01 15:06:56 +0300

    fix primary section description 1024px

--- src/components/Homepage/PrimarySection/index.js
+++ src/components/Homepage/PrimarySection/index.js
@@ -12,17 +12,21 @@ export const PrimarySection = () => {
   return (
     <div id="home" className="primary-section">
       <div className="container">
-        <h1 className="primary-section__title">Test assignment</h1>
-        <h1 className="primary-section__title">for Frontend</h1>
-        <h1 className="primary-section__title">Developer position</h1>
-        <p className="primary-section__description">
-          We kindly remind you that your test assignment should be submitted as
-          a link to github/bitbucket repository. Please be patient, we consider
-          and respond to every application that meets minimum requirements. We
-          look forward to your submission. Good luck! The photo has to scale in
-          the banner area on the different screens
-        </p>
-        <Button options={btnOptions} />
+        <div className="primary-section__subcontainer">
+          <div className="primary-section__content">
+            <h1 className="primary-section__title">Test assignment</h1>
+            <h1 className="primary-section__title">for Frontend</h1>
+            <h1 className="primary-section__title">Developer position</h1>
+            <p className="primary-section__description">
+              We kindly remind you that your test assignment should be submitted
+              as a link to github/bitbucket repository. Please be patient, we
+              consider and respond to every application that meets minimum
+              requirements. We look forward to your submission. Good luck! The
+              photo has to scale in the banner area on the different screens
+            </p>
+            <Button options={btnOptions} />
+          </div>
+        </div>
       </div>
     </div>
   );
